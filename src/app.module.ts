50cfcfe7f21319f06e24b16c929a35e5b079b1d0
Prasenjit Aluni <prasenjit.aluni@iqvia.com>
2026-01-06 09:21:31 +0530

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,6 @@ import { join } from 'path';
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
-      serveRoot: '/public',
     }),
     //MongooseModule.forRoot('mongodb+srv://tester:tester1234@cluster0.hlicuim.mongodb.net/demodb?retryWrites=true&w=majority'),
     //ProductModule,
