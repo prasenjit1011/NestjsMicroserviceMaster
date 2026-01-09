5ca7b1914b603b680a0900a2ae652e9e82bd65fc
Prasenjit Aluni <prasenjit.aluni@iqvia.com>
2026-01-09 19:10:21 +0530

    Commit

--- src/demat/demat.controller.ts
+++ src/demat/demat.controller.ts
@@ -49,6 +49,7 @@ export class DematController {
       const itemData = apiData.data.find(data => data.sid === sid);
       const apiPrice = itemData ? itemData.price.toFixed(0) : 'N/A';
       const dyChange = itemData ? itemData.dyChange.toFixed(0) : 'N/A';
+      const profit = itemData ? (apiPrice * item.qty - item.total).toFixed(0) : 'N/A';
       // console.log(apiPrice);
       // console.log('=====================');
 
@@ -64,6 +65,7 @@ export class DematController {
         <td class="total">₹${apiPrice}</td>
         <td class="qty">${item.qty || 0}</td>
         <td class="total">₹${(item.total || 0).toFixed(0)}</td>
+        <td class="total">₹${profit || 0}</td>
       </tr>
     `}).join('');
     
