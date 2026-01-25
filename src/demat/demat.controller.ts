fa18bc023f8864968a6c0c6c5ed9b4fee527a120
Prasenjit Aluni <prasenjit.aluni@iqvia.com>
2026-01-25 10:50:04 +0530

    Commit-04

--- src/demat/demat.controller.ts
+++ src/demat/demat.controller.ts
@@ -539,7 +539,7 @@ export class DematController {
         const latestDate = latestPoint ? new Date(latestPoint['ts']).toLocaleString() : 'N/A';
 
         // Read HTML template
-        const templatePath = getTemplatePath('analysis.html');
+        const templatePath = getTemplatePath('quarterly-high-low.html');
         let html = fs.readFileSync(templatePath, 'utf-8');
 
         // Prepare data
