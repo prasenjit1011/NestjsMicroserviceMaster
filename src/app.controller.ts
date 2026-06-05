be644bd310900ad8073911b8ffe74a19e72cc524
Mr. Aluni <prasenjit.aluni@gmail.com>
2026-06-05 21:59:49 +0530

    Portfolio Setup

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { AppService } from './app.service';
+import { getTemplatePath } from './utils/path.util';
 
 
-@Controller()
+@Controller('/')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/home')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/')
+  async getTableView(@Res() res: Response) {
+    const fs = require('fs');
+    const path = require('path');
+      // Read the HTML template
+      const templatePath = getTemplatePath('profile.html');
+      let html = fs.readFileSync(templatePath, 'utf8');
+      res.send(html);
   }
 }
