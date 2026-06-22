39253182a902a15dff22455ad2b3b964d6c19af6
Mr. Aluni <prasenjit.aluni@gmail.com>
2026-06-22 08:56:45 +0530

    Product Crud = 02

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,13 @@ import {
 
 import { ProductsModule } from './products/products.module';
 import { LoggerMiddleware } from './middleware/logger.middleware';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [ProductsModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule
   implements NestModule
