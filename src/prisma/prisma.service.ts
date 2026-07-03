a4acd437ae8ee2b8ff1348355363e5131f5534d1
Mr. Aluni <prasenjit.aluni@gmail.com>
2026-07-04 01:31:42 +0530

    225

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -4,7 +4,8 @@ import {
   OnModuleDestroy,
 } from '@nestjs/common';
 
-import { PrismaClient } from '../../generated/prisma/client';
+// import { PrismaClient } from '../../generated/prisma/client';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService
