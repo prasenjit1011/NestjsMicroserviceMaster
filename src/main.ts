2c10a71fa8c3ee29154e58170858f11e02388406
Mr. Aluni <prasenjit.aluni@gmail.com>
2026-07-03 19:28:57 +0530

    Commit

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,16 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:3000'
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
