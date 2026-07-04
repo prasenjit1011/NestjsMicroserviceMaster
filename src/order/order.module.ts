02ab4efed57fe71f0e329994688157ba35f34d66
Mr. Aluni <prasenjit.aluni@gmail.com>
2026-07-04 21:36:01 +0530

    Code updated

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
+import * as googleProtoFiles from 'google-proto-files';
 
 import { GRPC } from '../common/constants';
 import { OrderController } from './order.controller';
@@ -15,6 +16,17 @@ import { OrderService } from './order.service';
           url: GRPC.ORDER_URL,
           package: GRPC.ORDER_PACKAGE,
           protoPath: GRPC.ORDER_PROTO_PATH,
+
+          loader: {
+            keepCase: true,
+            longs: String,
+            enums: String,
+            defaults: true,
+            oneofs: true,
+            includeDirs: [
+              googleProtoFiles.getProtoPath(),
+            ],
+          },
         },
       },
     ]),
