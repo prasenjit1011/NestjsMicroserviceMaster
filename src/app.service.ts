8ee0708e239d3f8fa3fafb9122455b899bdb1597
Mr. Aluni <prasenjit.aluni@gmail.com>
2026-07-06 08:49:04 +0530

    C-123

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@nestjs/common';
 export class AppService {
   getHello() {
     return {
-      message: 'Hello World!',
+      message: 'Hello World From Item Service!',
     };
   }
 }
