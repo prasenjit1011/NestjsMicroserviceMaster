19787c068ab008813d7e299564b991d4eb15de5b
Mr. Aluni <prasenjit.aluni@gmail.com>
2026-07-07 09:12:06 +0530

    Order-052

--- src/app.service.ts
+++ src/app.service.ts
@@ -7,4 +7,15 @@ export class AppService {
       message: 'Hello World From Item Service!',
     };
   }
+
+  async getData(){
+    return {res:"item Data", data:[{id:1, title:"demo"},{id:2,title:"dummy"}]};
+
+    // return await this.prisma.item.findMany({
+    //   orderBy: {
+    //     id: 'asc',
+    //   },
+    // });
+  }
+
 }
