e8f2bf51374a7f81cbc9ce2e1f4785489967593d
Mr. Aluni <prasenjit.aluni@gmail.com>
2026-07-09 10:53:45 +0530

    456

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @GrpcMethod('ItemService', 'GetHello')
+  @GrpcMethod('OrderService', 'GetHello')
   getHello() {
     return this.appService.getHello();
   }
