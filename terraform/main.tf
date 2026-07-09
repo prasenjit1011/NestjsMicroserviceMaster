076ac6ba75b29cebbadf68ca1f5cddc99c9972c0
Mr. Aluni <prasenjit.aluni@gmail.com>
2026-07-09 10:42:33 +0530

    456

--- terraform/main.tf
+++ terraform/main.tf
@@ -76,7 +76,7 @@ resource "google_cloud_run_v2_service" "app" {
       image = "gcr.io/cloudrun/hello"
 
       ports {
-        container_port = 50051
+        container_port = 50052
       }
 
       env {
@@ -86,7 +86,7 @@ resource "google_cloud_run_v2_service" "app" {
 
       env {
         name  = "GRPC_URL"
-        value = "0.0.0.0:50051"
+        value = "0.0.0.0:50052"
       }
 
       resources {
@@ -102,13 +102,13 @@ resource "google_cloud_run_v2_service" "app" {
         failure_threshold = 24
 
         tcp_socket {
-          port = 50051
+          port = 50052
         }
       }
 
       liveness_probe {
         tcp_socket {
-          port = 50051
+          port = 50052
         }
 
         initial_delay_seconds = 20
