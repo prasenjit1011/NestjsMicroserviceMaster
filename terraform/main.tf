116f37e261cd6e839c9841838b00686a892780f1
Mr. Aluni <prasenjit.aluni@gmail.com>
2026-07-10 17:40:55 +0530

    Commit-02

--- terraform/main.tf
+++ terraform/main.tf
@@ -53,7 +53,7 @@ resource "google_cloud_run_v2_service" "app" {
     google_artifact_registry_repository.repo
   ]
 
-  name     = "api-gateway"
+  name     = "revest-api-gateway"
   location = "asia-south1"
 
   ingress = "INGRESS_TRAFFIC_ALL"
