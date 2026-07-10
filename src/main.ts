cb0497efcff625e68c2e25a194b296017103af3b
Mr. Aluni <prasenjit.aluni@gmail.com>
2026-07-10 22:20:19 +0530

    c

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
         options: {
           url: process.env.ITEM_GRPC_URL || '127.0.0.1:50051',
           package: 'item',
-          protoPath: join(process.cwd(), 'src/grpc/item.proto'),
+          protoPath: join(__dirname, 'proto/item.proto'),
         },
       },
     );
