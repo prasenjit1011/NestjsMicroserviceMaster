41a36d7b3ed70f97f948e13d85ec18a35f157866
Mr. Aluni <prasenjit.aluni@gmail.com>
2026-07-11 09:04:43 +0530

    Api-904

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! This is a NestJS microservice application.';
+    return 'Hello World! This is a NestJS microservice application. Trying to connect Order Service 904';
   }
 }
