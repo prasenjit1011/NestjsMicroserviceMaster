a52f1933707172dd51a636302f6c0ecdd0276be1
Mr. Aluni <prasenjit.aluni@gmail.com>
2026-07-19 13:16:28 +0530

    Common Proto

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
               options: {
                 url: process.env.ORDER_GRPC_URL || '127.0.0.1:50042',
                 package: 'order',
-                protoPath: join(__dirname, 'proto/order.proto'),
+                protoPath: join(process.cwd(), 'common-proto/proto/order.proto'),
               },
             },
           );
